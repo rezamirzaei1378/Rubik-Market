89d3002c5ebd8465e63da8362ca3229d04971785
Reza Mirzaie <r.mirzaie1378@gmail.com>
2024-12-03 18:15:55 +0330

    Initial project with clean architecture and add template

---
+++ Rubik-Market.Web/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rubik_Market.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        #region Register
+        [HttpGet("Register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        #endregion
+
+        #region Login
+        [HttpGet("Login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        #endregion
+
+    }
+}
---
+++ Rubik-Market.Web/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Rubik_Market.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
